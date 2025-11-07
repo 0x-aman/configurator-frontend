c8712022f54b36dddf53b68fad3880fb7e68efd0
WebDevAman <amanpande101@gmail.com>
2025-11-07 17:52:57 +0530

    embed js fixes

--- public/embed.js
+++ public/embed.js
@@ -1,13 +1,14 @@
 (function () {
   const script = document.currentScript;
-  const apiKey = script.getAttribute("data-key");
+  const publicId = script.getAttribute("data-public-id");
+  const publicKey = script.getAttribute("data-public-key");
   const container = document.createElement("div");
   container.id = "keen-iframe-container";
 
   const iframe = document.createElement("iframe");
-  iframe.src = `https://product-configurator-frontend.netlify.app/?apiKey=${encodeURIComponent(
-    apiKey
-  )}`;
+  iframe.src = `https://product-configurator-frontend.netlify.app/?publicId=${encodeURIComponent(
+    publicId
+  )}?publicKey=${encodeURIComponent(publicKey)}`;
   iframe.style.width = "100%";
   iframe.style.height = "600px";
   iframe.style.border = "none";
