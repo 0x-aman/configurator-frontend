7143639bb6eea35d0063a8dd9e1f7e9f6320c98b
WebDevAman <amanpande101@gmail.com>
2025-11-20 13:50:50 +0530

    chore(assets): update embed script and public assets

--- public/embed.js
+++ public/embed.js
@@ -1,26 +1,17 @@
 (function () {
   const script = document.currentScript;
-  const publicId = script.getAttribute("data-public-id");
-  const publicKey = script.getAttribute("data-public-key");
+  const apiKey = script.getAttribute("data-key");
   const container = document.createElement("div");
   container.id = "keen-iframe-container";
 
   const iframe = document.createElement("iframe");
-  iframe.src = `https://product-configurator-frontend.netlify.app/?publicId=${encodeURIComponent(
-    publicId
-  )}&publicKey=${encodeURIComponent(publicKey)}`;
+  iframe.src = `https://keen-daffodil-bf4586.netlify.app/?apiKey=${encodeURIComponent(
+    apiKey
+  )}`;
   iframe.style.width = "100%";
   iframe.style.height = "600px";
   iframe.style.border = "none";
 
   container.appendChild(iframe);
   script.parentNode.insertBefore(container, script.nextSibling);
-
-  // ✅ send client origin to iframe
-  iframe.onload = () => {
-    iframe.contentWindow.postMessage(
-      { embedOrigin: window.location.origin },
-      "https://product-configurator-frontend.netlify.app"
-    );
-  };
 })();
